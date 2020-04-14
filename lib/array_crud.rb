719a531d03e3a33ee96aeb3726dae41addbce872
Mark Cascardi <mcascardi@gmail.com>
2020-04-14 20:02:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,11 +13,12 @@ end
 
 def add_element_to_start_of_array(array, element)
   array = [1, 2]
-  array.unshift(element) 
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-
+  array = [1, 2]
+  array.pop(element)
 end
 
 def remove_element_from_start_of_array(array)
