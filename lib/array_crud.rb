2c83a2b22e142529f91f2f89e1a317a3602fc95c
Mark Cascardi <mcascardi@gmail.com>
2020-04-14 20:05:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -31,9 +31,11 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-
+  array = [1, 2]
+  array.first
 end
 
 def retrieve_last_element_from_array(array)
-
+  array = [1, 2]
+  array.last
 end
